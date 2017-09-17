f030351e8573c95858ecb049dcaf181db5bb6c8d
Junlong Wang <wjlinnz@gmail.com>
2017-09-17 22:39:58 +1200

    Initial routes file: userRoute, postRoute

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,1 +1,12 @@
-ECHO is on.
+'use strict';
+const postController = require('../controllers/postContrller');
+const router = require('router');
+const mysql = require('mysql');
+const pool = mysql.createPool({
+				connectionLimit	: 100,
+				host						: 'localhost',
+				user						: 'root',
+				password				: '',
+				database				: 'blogging'
+			});
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,12 @@
-ECHO is on.
+'use strict';
+const userController = require('../controllers/userContrller');
+const router = require('router');
+const mysql = require('mysql');
+const pool = mysql.createPool({
+				connectionLimit	: 100,
+				host						: 'localhost',
+				user						: 'root',
+				password				: '',
+				database				: 'blogging'
+			});
+
