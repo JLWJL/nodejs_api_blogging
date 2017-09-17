70da70a6002c5b1e06351b573523fb39c7fb9aba
Junlong Wang <wjlinnz@gmail.com>
2017-09-17 22:36:15 +1200

    Initial app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-ECHO is on.
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const userRoute = require('./routes/userRoute');
+const postRoute = require('./routes/postRoute');
+const port  = process.env.PORT||3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+
+
+
+
+/**
+* Error handling
+*/
+app.use((req,res, next)=>{
+	
+	console.log("not found");
+	res.sendFile(__dirname+'/public/Error.html');
+});
+
+
+app.listen(port, ()=>{
+	console.log(`${port} listening`);
+}));
